40bd4a7180175c2dc6a4c16d0ed98c6d3c780df4
YigitEfe3 <efe.yigit.efee@gmail.com>
2022-05-10 15:05:15 +0300

    za

--- app/src/main/java/bitirme/proje/musicrecommendation/ProfileActivity.java
+++ app/src/main/java/bitirme/proje/musicrecommendation/ProfileActivity.java
@@ -1,8 +1,10 @@
 package bitirme.proje.musicrecommendation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,12 +15,15 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private ImageView HomeImg,CamImg,LibImg;
     private TextView HomeText,CamText,LibText;
-
+    private TextView songArtist,songName;
 
 
     @Override
@@ -36,7 +41,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         HomeImg.setOnClickListener(this);
         CamImg.setOnClickListener(this);
         LibImg.setOnClickListener(this);
-
+   ;
 
 
 
@@ -78,6 +83,28 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.LibraryDirect:
                 break;
 
+
+        }
+
+    }
+
+
+
+    public void play_song(View v){
+        MediaPlayer mediaPlayer=new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/musicrecommendation-a57f0.appspot.com/o/Ava%20Max%20-%20Sweet%20but%20Psycho%20(Lyrics).mp3?alt=media&token=345d13c2-2560-41d6-b8da-0656b3e24a27");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+
+            mediaPlayer.prepare();
+
+        }catch (IOException e){
+            e.printStackTrace();
         }
 
     }
